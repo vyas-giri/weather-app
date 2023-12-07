47e56902b351c33a206a83ba3484b6e6f9a34c1c
Vyas Giri <vyasgiri25@gmail.com>
2023-12-07 15:47:42 +0530

    Changed units

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ function App() {
   const [data, setData] = useState({})
   // const [loading, setLoading] = useState(true)
   const [er, setEr] = useState(false)
-  const main_url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
+  const main_url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
 
   const searchLocation = async (event) => {
     if (event.key === 'Enter') {
       try {
         await axios.get(main_url).then((response) => {
           setData(response.data)
+          setEr(false)
         });
       }
       catch (err) {
         console.error(err)
         console.error(err.response.data)
-        if (err.response.data.cod === 404) {
+        if (err.response.data.cod === '404') {
           setEr(true)
         }
       }
@@ -38,35 +39,35 @@ function App() {
         type='text'
         onKeyDown={searchLocation}/>
       </div>
-      {data.cod === 404 && <div>
-        <span className='text-6xl text-cyan-600'>Did you type that correctly?</span>
-        <span className='text-3xl'>Make sure you are searching a valid place!</span>
+      {er && <div className='flex flex-col items-center justify-center space-y-3'>
+        <span className='text-xl sm:text-5xl text-purple-700'>Did you type that correctly?</span>
+        <span className='text-sm sm:text-2xl'>Make sure you are searching for a valid place!</span>
       </div>}
       {!data.main && !er && <div className='flex justify-center items-center flex-col p-5 space-y-3'>
-        <span className='text-6xl text-violet-700'>Weather App</span>
-        <span>Search for any place above!</span>
+        <span className='text-4xl sm:text-5xl text-violet-700'>Weather App</span>
+        <span className='text-md'>Search for any place above!</span>
       </div>}
-      {data.main && <div className='-space-y-36'>
+      {data.main && !er && <div className='-space-y-36'>
       <div className='placeandtemp flex flex-col justify-center items-center space-y-3 text-amber-900 font-bold'>
         <span className='text-4xl mt-50'>{data.name}</span>
-        <span className='text-2xl'>{data.main.temp}&deg; F</span>
-        <span className='text-md'>Feels like {data.main.feels_like}&deg; F</span>
+        <span className='text-2xl'>{data.main.temp}&deg; C</span>
+        <span className='text-md'>Feels like {data.main.feels_like}&deg; C</span>
         <span className='text-xl'>{data.weather[0].description}</span>
         <span className='text-lg'>Humidity: {data.main.humidity}</span>
-        <div className='flex justify-between space-x-72'>
+        <div className='flex justify-between space-x-5 sm:space-x-72'>
           <div className='flex justify-center items-center flex-col'>
           <p className=''>MIN</p>
-          <p>{data.main.temp_min}&deg; F</p>
+          <p>{data.main.temp_min}&deg; C</p>
           </div>
           <div className='flex justify-center items-center flex-col'>
           <p className=''>MAX</p>
-          <p>{data.main.temp_max}&deg; F</p>
+          <p>{data.main.temp_max}&deg; C</p>
           </div>
         </div>
       </div>
-      <div className='float-right flex flex-col space-y-16 items-center justify-center max-w-screen'>
-        <span className='-rotate-90 text-cyan-500'>Wind: {data.wind.speed}mph</span>
-        <img className='-rotate-90 pr-2' width="48" height="48" src="https://img.icons8.com/fluency/48/wind.png" alt="wind"/>
+      <div className='float-right flex flex-col justify-end items-end space-y-16 w-auto'>
+        <span className='-rotate-90 text-cyan-500 float-right -mr-10'>Wind: {data.wind.speed} mph</span>
+        <img className='-rotate-90 sm:pr-2 float-right mr-1' width="48" height="48" src="https://img.icons8.com/fluency/48/wind.png" alt="wind"/>
       </div>
       </div>}
 
